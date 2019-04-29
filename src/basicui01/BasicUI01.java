1dad109df63762d75ba4d8de8fb3d165d73e33ce
hendroprwk08 <hendroprwk08@gmail.com>
2019-04-29 21:08:12 +0700

    ok

---
+++ src/basicui01/BasicUI01.java
@@ -0,0 +1,10 @@
+package basicui01;
+
+public class BasicUI01 {
+
+    public static void main(String[] args) {
+        //menampilkan form
+        FLatih01 f = new FLatih01();
+        f.setVisible(true);
+    }    
+}
